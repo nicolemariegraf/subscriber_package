8c39c9f4fd8afdc074a2a8971df87b4615bb2764
nicolemariegraf <nmg63@case.edu>
2019-09-11 15:58:04 -0700

    Update subscriber package

--- src/subscriber_code.cpp
+++ src/subscriber_code.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "publisher_package/topic_name.h"
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
 ROS_INFO("I heard: [%s]", msg->data.c_str());
@@ -9,7 +10,7 @@ int main(int argc, char **argv)
 {
 ros::init(argc, argv , "subscriber_node");
 ros::NodeHandle n;
-ros::Subscriber sub = n.subscribe("publisher_topic", 1000, chatterCallback);
+ros::Subscriber sub = n.subscribe("topic_name", 1000, chatterCallback);
 ros::spin();
 
 
